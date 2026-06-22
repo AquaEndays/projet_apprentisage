03036657f54ab5edf08be36b5f740c13d2105c2e
AquaEndays <juniorkengne861@gmail.com>
2026-06-22 01:55:25 +0100

    Version 1.1 : Amelioré

--- Assets/CodeMouvement.cs
+++ Assets/CodeMouvement.cs
@@ -4,6 +4,9 @@ public class CodeMouvement : MonoBehaviour
     
     public CharacterController moteurDeDeplacement;
     public Animator monAnimateur;
+    private float vitesseVerticale = 0f;
+    public float forceDeSaut = 6f;
+    public float gravite = -9.81f;
     float vitesse = 4f;
 
     void Start()
@@ -19,10 +22,42 @@ public class CodeMouvement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = new Vector3(x, 0, z);
-        moteurDeDeplacement.Move(move *vitesse* Time.deltaTime);
-// Prends 0.15 seconde pour faire grimper la valeur proprement
-monAnimateur.SetFloat("Speed", z, 0.15f, Time.deltaTime);
-monAnimateur.SetFloat("Direction", x, 0.15f, Time.deltaTime);
+        if (move.magnitude > 0)
+        {
+            transform.forward = move;
+            moteurDeDeplacement.Move(move *vitesse* Time.deltaTime);
+            // Prends 0.15 seconde pour faire grimper la valeur proprement
+            monAnimateur.SetFloat("Speed", z, 0.05f, Time.deltaTime);
+            //monAnimateur.SetFloat("Direction", x, 0.15f, Time.deltaTime);
+        }
+        else
+        {
+            monAnimateur.SetFloat("Speed", 0, 0.15f, Time.deltaTime);
+            //monAnimateur.SetFloat("Direction", 0, 0.15f, Time.deltaTime);
+        }
+
+        if (moteurDeDeplacement.isGrounded)
+        {
+            vitesseVerticale = -2f;
+            if(Input.GetButtonDown("Jump"))
+            {
+                vitesseVerticale = forceDeSaut;
+                monAnimateur.SetTrigger("Jump");
+            }
+        }else
+        {
+            vitesseVerticale += gravite * Time.deltaTime;
+        }
+        Vector3 saut = new Vector3(0, vitesseVerticale, 0);
+        moteurDeDeplacement.Move(saut * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) && move.magnitude > 0)
+        {
+            monAnimateur.SetTrigger("Slide");
+        }
+
+
+ 
     }
 
 }
